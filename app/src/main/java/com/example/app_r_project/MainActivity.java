863815418c74f3a2ae714b24a6136b0a1db95989
valick18 <valick18@gmail.com>
2021-02-21 13:15:23 +0200

    Marker on Map

--- app/src/main/java/com/example/app_r_project/MainActivity.java
+++ app/src/main/java/com/example/app_r_project/MainActivity.java
@@ -6,42 +6,33 @@ import android.os.Bundle;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
 
     GoogleMap gMap;
 
-    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // Retrieve the content view that renders the map.
         setContentView(R.layout.activity_main);
-
-
-        SupportMapFragment supportMapFragment = (SupportMapFragment)
-                getSupportFragmentManager().findFragmentById(R.id.google_map);
-
+        // Get the SupportMapFragment and request notification
+        // when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.google_map);
+        mapFragment.getMapAsync(this);
     }
 
+
+
     public void onMapReady(GoogleMap googleMap) {
-
-        gMap = googleMap;
-
-        gMap.setOnMapClickListener(latLng -> {
-       MarkerOptions markerOptions =new MarkerOptions();
-
-       markerOptions.position(latLng);
-
-       markerOptions.title(latLng.latitude+" : "+latLng.longitude);
-
-       gMap.clear();
-
-       gMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng,10));
-
-       gMap.addMarker(markerOptions);
-        });
+        LatLng sydney = new LatLng(-33.852, 151.211);
+        googleMap.addMarker(new MarkerOptions()
+                .position(sydney)
+                .title("Marker in Sydney"));
     }
 }
 
